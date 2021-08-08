0a3257650bad7889fd53b220ea1d54bbbb4a8917
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-08 15:29:52 +0300

    Created 'UI' class

--- script.js
+++ script.js
@@ -8,6 +8,39 @@ class Employee {
 }
 
 // UI Class - handles the displaying of the data
+class UI {
+    static displayEmployees() {
+        const StoredEmployees = [
+            {
+                firstName: 'Cosmin',
+                lastName: 'Jescu',
+                emailAddress: 'jescucosmin99@gmail.com'
+            },
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                emailAddress: 'johndoe@gmail.com'
+            }
+        ];
+        const employees = StoredEmployees;
+
+        employees.forEach((employee) => UI.addEmployeeToList(employee));
+    }
+
+    //adds a new table row
+    static addEmployeeToList(employee) {
+        const list = document.querySelector('#employee-list');
+
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.emailAddress}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;  //delete button
+
+        list.appendChild(row);
+    }
+}
+
 
 // Store Class - handles local storage
 
