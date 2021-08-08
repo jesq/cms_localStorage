7fad7564f8d8e8ea199a3239e0f2b63c241c367e
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-08 15:18:37 +0300

    Created "Employee" class

---
+++ script.js
@@ -0,0 +1,18 @@
+// Employee Class - represents an employee
+class Employee {
+    constructor(firstName, lastName, emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
+}
+
+// UI Class - handles the displaying of the data
+
+// Store Class - handles local storage
+
+// Event: Display employees
+
+// Event: Add an employee
+
+// Event: Remove an employee
